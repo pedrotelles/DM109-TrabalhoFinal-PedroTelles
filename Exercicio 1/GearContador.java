fe6039d9b3ebc9d7c4f299116826dbb08da74c58
pedrotelles <pedrotelles@gec.inatel.br>
2018-01-12 01:00:44 -0200

    removed folder src

--- Exercicio 1/GearContador.java
+++ Exercicio 1/GearContador.java
@@ -72,7 +72,7 @@ public class GearContador {
       public void flatMap(ObjectNode jsonTelemetry, Collector<Tuple3<String, Float, Integer>> out) throws Exception {
         String carNumber = jsonTelemetry.get("Car").asText();
         float gear = jsonTelemetry.get("telemetry").get("Gear").floatValue();
-        if(carNumber.intern() == "3"){
+        if(carNumber.equals("3")){
           out.collect(new Tuple3<>(carNumber, gear, 0 ));
         }
       }
